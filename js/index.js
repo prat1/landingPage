3f6931bb32214c51c40b9a9e78bbc577e3e60431
Prathyusha-Pallapati <pratgermany@gmail.com>
2020-12-20 14:17:40 +0100

    adding scroll and active active class functionality.

--- js/index.js
+++ js/index.js
@@ -3,77 +3,60 @@
 const sections = document.querySelectorAll("section");
 let listItem;
 let navigationLink;
-console.log(sections);
+
+//helper function to check if the section is in view port or not
+// reference: https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/#:~:text=If%20an%20element%20is%20in%20the%20viewport%2C%20it's%20position%20from,the%20height%20of%20the%20viewport.
+
+
+var isVisible = function (elem) {
+    var boundry = elem.getBoundingClientRect();
+    return (
+        boundry.top >= 0 &&
+        boundry.left >= 0 &&
+        boundry.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        boundry.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
 for(section of sections){
   console.log(section);
    listItem = document.createElement('li');
-   navigationLink = document.createElement('a');
-   console.log(navigationLink);
+   //navigationLink = document.createElement('a');
+   //console.log(navigationLink);
    //getting the text data from sections
-   listItem.innerHTML= section.dataset.sectionName ;
+   listItem.innerHTML= `<a class="menu__link ${section.id}" href="#${section.id}">${section.dataset.sectionName}</a>`;
    console.log(listItem);
    //giving properties to the anchor tag
-   navigationLink.setAttribute("class","anchorLink");
+   //navigationLink.setAttribute("class","anchorLink");
    //navigationLink.setAttribute("href",`#${section.id}`);
-   navigationLink.href ="index.html#"+`${section.id}`;
-   navigationLink.setAttribute("id",section.id);
-   navigationLink.textContent = section.dataset.sectionName; 
+   //navigationLink.href ="index.html#"+`${section.id}`;
+   //navigationLink.setAttribute("id",section.id);
+   //navigationLink.textContent = section.dataset.sectionName;
 
-   listItem.appendChild(navigationLink);
+   //listItem.appendChild(navigationLink);
    navList.appendChild(listItem);
 
 }
 
-console.log(navList);
-
-
-/*
-const navbarMenu = document.getElementById("navList");
-//sections = document.querySelectorAll("section");
-function createNav(){
-for (let item of sections){
-let section = document.createElement("li");
-section.className = "menu__link";
-console.log(section.className);
-section.dataset.nav = item.id;
-console.log(section.dataset.nav);
-section.innerText = item.dataset.nav;
-console.log(section.innerText)
-navbarMenu.appendChild(section);
-};};
-createNav();
-//const navList = document.getElementById('navList');
 //console.log(navList);
-//Helper function to check if an element is in viewport
-//if in viewport returns true
-/*function isInViewport(element) {
-    const distance = element.getBoundingClientRect();
-    return (
-        distance.top <= 100 &&
-        distance.left >= 0 &&
-        distance.bottom >= 90 &&
-        distance.right <= (window.innerWidth || document.documentElement.clientWidth));
-}
+//document.querySelector(`.${section.id}`);
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
-// build the nav menu
-//function buildNavBar () {
-  /*  const fragment = document.createDocumentFragment();
-    for (const section of sections) {
-        let newNavItem = document.createElement('li');
-        const id = section.id;
-        console.log(id);
-        const dataNav = section.dataset.nav;
-        comsole.log(dataNav);
-        newNavItem.innerHTML = `<a class="menu__link ${id}" href="#${id}">${dataNav}</a>`;
-        fragment.appendChild(newNavItem);
-    }
-    navBar.appendChild(fragment);
-}
 
-buildNavBar();*/
+
+
+//Adding event listner for the scrolling action, checking if element is in viewport, also adding active class for sections and links.
+// reference: https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/#:~:text=If%20an%20element%20is%20in%20the%20viewport%2C%20it's%20position%20from,the%20height%20of%20the%20viewport.
+
+document.addEventListener('scroll', function sectionActive(){
+for (section of sections){
+  const currentNavItem = document.querySelector(`.${section.id}`);
+  if(isVisible(section)){
+    section.classList.add("activeSection"); //adding active class for section
+    currentNavItem.classList.add("activeLink"); // adding active class for link
+      console.log(`current active section is ${section.id}`);
+  } else{
+    section.classList.remove("activeSection");
+    currentNavItem.classList.remove("activeLink");
+  }
+}
+});
